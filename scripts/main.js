301ce7e396036f28a3ae8da5986c35951dad5884
Matt Chastain <matthewdchastain@gmail.com>
2015-05-28 14:36:08 -0400

    User authentication added

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,17 @@
 
   $(document).ready(function(){
     $('body').prepend(JST['application']());
+   	var code = window.location.href.match(/\?code=(.*)/)[1];
+   	if (code) {
+   		$.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
+		 console.log(data.token);
+		});
+   	}
   });
+
+
+  $('button').on('click', function(e) {
+  	window.location.replace('https://github.com/login/oauth/authorize?client_id=c76a5cce9a1d3c44517e');
+  });
+
 })();
